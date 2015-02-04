8aa21fc6f88ef3c306cb60a73c48ff6a8fc43a7e
carl-simpson-68 <carl.simpson@apps4.co.uk>
2015-02-04 15:39:22 +0000

    stage one working

---
+++ js/leafletEmbed.js
@@ -0,0 +1,129 @@
+
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers=[];
+
+function initmap() {
+	// set up AJAX request
+	ajaxRequest = GetXmlHttpObject();
+	if (ajaxRequest==null) {
+		alert ("This browser does not support HTTP Request");
+		return;
+	}
+	// set up the map
+	map = new L.Map('map');
+
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});		
+
+	// start the map in South-East England
+	map.setView(new L.LatLng(53.366802, -2.264599),8);
+	map.addLayer(osm);
+
+	askForPlots();
+	map.on('moveend', onMapMove);
+}
+function onMapMove(e) { askForPlots(); }
+
+function GetXmlHttpObject() {
+	if (window.XMLHttpRequest) { return new XMLHttpRequest(); }
+	if (window.ActiveXObject)  { return new ActiveXObject("Microsoft.XMLHTTP"); }
+	return null;
+}
+
+function askForPlots() {
+	// request the marker info with AJAX for the current bounds
+	var bounds=map.getBounds();
+	var minll=bounds.getSouthWest();
+	var maxll=bounds.getNorthEast();
+	var msg='js/test.json';
+	ajaxRequest.onreadystatechange = stateChanged;
+	ajaxRequest.open('GET', msg, true);
+	ajaxRequest.send(null);
+}
+
+function stateChanged() {
+	// if AJAX returned a list of markers, add them to the map
+	if (ajaxRequest.readyState==4) {
+		//use the info here that was returned
+		if (ajaxRequest.status==200) {
+			plotlist=eval("(" + ajaxRequest.responseText + ")");
+			removeMarkers();
+			for (i=0;i<plotlist.length;i++) {
+				var plotll = new L.LatLng(plotlist[i].lat,plotlist[i].lon, true);
+				var plotmark = new L.Marker(plotll);
+				plotmark.data=plotlist[i];
+				map.addLayer(plotmark);
+				plotmark.bindPopup("<h3>"+plotlist[i].name+"</h3>"+plotlist[i].details);
+				plotlayers.push(plotmark);
+			}
+		}
+	}
+}
+
+function removeMarkers() {
+	for (i=0;i<plotlayers.length;i++) {
+		map.removeLayer(plotlayers[i]);
+	}
+	plotlayers=[];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var map = L.map('map').setView([53.366904, -2.265887], 13);
+
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	id: 'examples.map-i875mjb7'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+	.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+L.circle([51.508, -0.11], 500, {
+	color: 'red',
+	fillColor: '#f03',
+	fillOpacity: 0.5
+}).addTo(map).bindPopup("I am a circle.");
+
+L.polygon([
+	[51.509, -0.08],
+	[51.503, -0.06],
+	[51.51, -0.047]
+]).addTo(map).bindPopup("I am a polygon.");
+
+
+var popup = L.popup();
+
+function onMapClick(e) {
+	popup
+		.setLatLng(e.latlng)
+		.setContent("You clicked the map at " + e.latlng.toString())
+		.openOn(map);
+}
+
+map.on('click', onMapClick);
+*/
---
+++ js/testData.js
@@ -0,0 +1,16 @@
+myFunction([
+{
+  "name":"Apps4",
+  "lon":"-2.265597",
+  "lat":"53.367211",
+  "details":"teh office.",
+  "url":"walkone.html"
+},
+{
+  "name":"Random Park",
+  "lon":"-2.128565",
+  "lat":"53.232331",
+  "details":"blurb at test location. Leaflet is a new JavaScript library for embedding maps which is quickly gaining popularity. Simpler and smaller than OpenLayers, it is a good choice for those with fairly standard embedding needs.",
+  "url":"www.carlsimpson.co.uk"
+}
+]);
